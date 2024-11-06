11da196e8c55188c2ee40e3ce429bbd0f3919978
today-is-first <diligent0644@gmail.com>
2024-11-06 17:57:22 +0900

    design: 비회원 로그인 버튼 컴포넌트 구현

---
+++ packages/frontend/src/components/landing/GuestLoginButton.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/common/Button';
+function GuestLoginButton() {
+  function handleClick() {}
+  return (
+    <>
+      <Button
+        className="bg-surface-default text-default ring-1 ring-grayscale-400"
+        onClick={handleClick}
+        buttonText="비회원으로 시작하기"
+      />
+    </>
+  );
+}
+export default GuestLoginButton;
