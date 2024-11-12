0d55db040196d3ef13c1d6b4270133ea91f0759c
zizonyoungjun <naya700@naver.com>
2024-11-12 14:57:54 +0900

    feat: 게임 생성 시 UUID 생성 후 동적 라우팅

--- packages/frontend/src/components/lobbyPage/RoomCreationButton.tsx
+++ packages/frontend/src/components/lobbyPage/RoomCreationButton.tsx
@@ -1,11 +1,15 @@
 import Button from '@/components/common/Button';
 import { useNavigate } from 'react-router-dom';
+import generateUUID from '@/utils/generateUUID';
 
 export default function RoomCreationButton() {
   const navigate = useNavigate();
+
   function handleClick() {
-    navigate('/game');
+    const gameId = generateUUID();
+    navigate(`/game/${gameId}`);
   }
+
   return (
     <Button
       className="w-4/5 text-xl font-semibold text-black bg-white hover:bg-gray-300"
