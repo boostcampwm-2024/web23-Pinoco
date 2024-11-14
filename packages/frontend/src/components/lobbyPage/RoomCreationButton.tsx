0fb95f9ac57c1b73c4d0444e4f18b2f05e3623f8
zizonyoungjun <naya700@naver.com>
2024-11-14 15:25:01 +0900

    feat: 방 생성 버튼에 useCreateRoom 추가

--- packages/frontend/src/components/lobbyPage/RoomCreationButton.tsx
+++ packages/frontend/src/components/lobbyPage/RoomCreationButton.tsx
@@ -1,15 +1,13 @@
 import Button from '@/components/common/Button';
-import { useNavigate } from 'react-router-dom';
+import useCreateRoom from '@/hooks/useCreateRoom';
 
 export default function RoomCreationButton() {
-  const navigate = useNavigate();
-  function handleClick() {
-    navigate('/game');
-  }
+  const { createRoom } = useCreateRoom();
+
   return (
     <Button
       className="w-4/5 text-xl font-semibold text-black bg-white hover:bg-gray-300"
-      onClick={handleClick}
+      onClick={createRoom}
       buttonText="게임 생성하기"
     />
   );
