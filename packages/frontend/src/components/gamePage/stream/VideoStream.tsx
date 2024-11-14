afa813acc58cb3591bf49a28d5067e53026dbbe4
Cllaude99 <xodbsrla1025@naver.com>
2024-11-14 17:26:49 +0900

    chore: build에러 수정

--- packages/frontend/src/components/gamePage/stream/VideoStream.tsx
+++ packages/frontend/src/components/gamePage/stream/VideoStream.tsx
@@ -1,19 +1,15 @@
 import React, { useRef, useEffect } from 'react';
-import { useMediaStream } from '@/hooks/UseMediaStream';
+import useMediaStream from '@/hooks/useMediaStream';
 
 export default function VideoStream() {
-    const { mediaStream, error } = UseMediaStream();
-    const videoRef = useRef<HTMLVideoElement>(null);
+  const { mediaStream, error } = useMediaStream();
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-    useEffect(() => {
-        if (videoRef.current && mediaStream) {
-            videoRef.current.srcObject = mediaStream;
-        }
-    }, [mediaStream]);
+  useEffect(() => {
+    if (videoRef.current && mediaStream) {
+      videoRef.current.srcObject = mediaStream;
+    }
+  }, [mediaStream]);
 
-    return (
-        <div>
-            {error ? <p>{error}</p> : <video ref={videoRef} autoPlay />}
-        </div>
-    );
+  return <div>{error ? <p>{error}</p> : <video ref={videoRef} autoPlay />}</div>;
 }
