9ecd98133ae9c3bc63402846f546154121e447a2
zizonyoungjun <naya700@naver.com>
2024-11-19 15:38:26 +0900

    chore: Rollup에 svg 명시적 처리 추가

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,5 +5,23 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
-  assetsInclude: ['**/*.svg'],
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'resolve-svg',
+          resolveId(source) {
+            if (source.endsWith('.svg')) return source;
+            return null;
+          },
+          load(id) {
+            if (id.endsWith('.svg')) {
+              return `export default ${JSON.stringify(id)}`;
+            }
+            return null;
+          },
+        },
+      ],
+    },
+  },
 });
