4a6699d286c1a936c8c3bcd9c0acebf99e9e70c4
today-is-first <diligent0644@gmail.com>
2024-11-19 15:54:52 +0900

    feat: Room 참가, 생성 로직 추가 구현

--- packages/backend/signalingServer/src/handlers/handleSocket.ts
+++ packages/backend/signalingServer/src/handlers/handleSocket.ts
@@ -1,25 +1,9 @@
-import { IWebRTCPayload } from '@/types/signaling.types';
 import { Socket } from 'socket.io';
-import { v4 as uuidv4 } from 'uuid';
+import { SOCKET_MESSAGES } from '@/constants/socketConstants';
 
 const handleSocket = (socket: Socket) => {
-  socket.on('create_room', () => {
-    const ssid = uuidv4();
-    socket.join(ssid);
-    socket.emit('create_room_success', {
-      ssid,
-      isHost: true,
-    });
-  });
-  socket.on('join_room', (payload: IWebRTCPayload) => {
-    socket.join(payload.ssid);
-    socket.emit('join_room_success', {
-      ssid: payload.ssid,
-      isHost: false,
-    });
-  });
   socket.on('disconnect', () => {
-    console.log('User Socket disconnected');
+    console.log(SOCKET_MESSAGES.disconnect);
   });
 };
 
