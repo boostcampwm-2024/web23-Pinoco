56d08ca03e9018abfb836df8494170500c2bce8e
today-is-first <diligent0644@gmail.com>
2024-11-19 15:53:58 +0900

    chore: 매직넘버, 메세지 상수화

---
+++ packages/backend/signalingServer/src/constants/roomConstants.ts
@@ -0,0 +1,8 @@
+export const ROOM_CONSTANTS = {
+  maxParticipants: 6,
+} as const;
+
+export const ROOM_ERROR_MESSAGES = {
+  notFound: '존재하지 않는 방입니다.',
+  full: '방이 가득 찼습니다.',
+} as const;
---
+++ packages/backend/signalingServer/src/constants/socketConstants.ts
@@ -0,0 +1,4 @@
+export const SOCKET_MESSAGES = {
+  connection: '클라이언트가 연결되었습니다.',
+  disconnect: '클라이언트가 연결이 끊어졌습니다.',
+} as const;
