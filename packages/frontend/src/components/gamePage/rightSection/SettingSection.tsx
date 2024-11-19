81a7ca7298530bba54cc1257e7165ba7dbd61c5a
Cllaude99 <xodbsrla1025@naver.com>
2024-11-19 18:35:01 +0900

    fix: 채팅 연동 로직 수정

--- packages/frontend/src/components/gamePage/rightSection/SettingSection.tsx
+++ packages/frontend/src/components/gamePage/rightSection/SettingSection.tsx
@@ -1,8 +1,20 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSocketStore } from '@/states/store/socketStore';
 import CameraOn from '@/assets/images/CameraOn.svg?react';
 import MikeOn from '@/assets/images/MikeOn.svg?react';
 import Leave from '@/assets/images/Leave.svg?react';
 
 export default function SettingSection() {
+  const navigate = useNavigate();
+  const socket = useSocketStore((state) => state.socket);
+
+  const handleLeave = () => {
+    if (!socket) return;
+
+    socket.emit('leave_room');
+    navigate('/lobby');
+  };
+
   return (
     <div className="flex justify-around p-4 bg-black rounded-lg opacity-80">
       <button className="px-4 py-4 bg-transparent rounded-lg">
@@ -11,7 +23,7 @@ export default function SettingSection() {
       <button className="px-4 py-2 bg-transparent rounded-lg">
         <CameraOn className="text-white-default" />
       </button>
-      <button className="px-4 py-2 bg-transparent rounded-lg">
+      <button className="px-4 py-2 bg-transparent rounded-lg" onClick={handleLeave}>
         <Leave className="text-white-default" />
       </button>
     </div>
