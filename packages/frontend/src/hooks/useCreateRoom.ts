c8cbea7599efb3f16ebcdd8516551d737c6c5ccf
today-is-first <diligent0644@gmail.com>
2024-11-20 18:18:32 +0900

    feat: create_room 시그널링 소켓 연결 추가

--- packages/frontend/src/hooks/useCreateRoom.ts
+++ packages/frontend/src/hooks/useCreateRoom.ts
@@ -2,12 +2,14 @@ import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '@/states/store/authStore';
 import { useRoomStore } from '@/states/store/roomStore';
 import { useSocketStore } from '@/states/store/socketStore';
+import { useSignalingSocketStore } from '@/states/store/signalingSocketStore';
 
 export default function useCreateRoom() {
   const navigate = useNavigate();
   const userId = useAuthStore((state) => state.userId);
   const setRoomData = useRoomStore((state) => state.setRoomData);
   const socket = useSocketStore((state) => state.socket);
+  const signalingSocket = useSignalingSocketStore((state) => state.signalingSocket);
 
   function handleCreateRoom() {
     if (!userId || !socket) return;
@@ -17,10 +19,15 @@ export default function useCreateRoom() {
     socket.on('create_room_success', (data: { gsid: string; isHost: boolean }) => {
       setRoomData(data.gsid, data.isHost);
       navigate(`/game/${data.gsid}`);
+      signalingSocket?.emit('create_room', { gsid: data.gsid });
     });
 
     socket.on('error', (data: { errorMessage: string }) => {
       alert(data.errorMessage);
+    });
+
+    signalingSocket?.on('error', (data: { errorMessage: string }) => {
+      alert(`방 생성 시 화면 공유에 실패했습니다: ${data.errorMessage}`);
     });
 
     return () => {
