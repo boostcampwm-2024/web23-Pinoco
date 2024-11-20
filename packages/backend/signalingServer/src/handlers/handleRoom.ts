bc13598ac82961b6e226ca02eca0dd692512c63e
today-is-first <diligent0644@gmail.com>
2024-11-20 18:20:39 +0900

    feat: 방 참가, 생성 검증 및 로그 출력 기능 추가

--- packages/backend/signalingServer/src/handlers/handleRoom.ts
+++ packages/backend/signalingServer/src/handlers/handleRoom.ts
@@ -1,18 +1,32 @@
 import { IWebRTCPayload } from '@/types/signaling.types';
-import { v4 as uuidv4 } from 'uuid';
 import { Socket, Server } from 'socket.io';
 import { ROOM_ERROR_MESSAGES, ROOM_CONSTANTS } from '@/constants/roomConstants';
+import { logRoomStatus, LogType } from '@/util/logUtils';
+import { getRoom, getRoomList } from '@/util/handlerUtils';
 
 const handleRoom = (socket: Socket, io: Server) => {
-  socket.on('create_room', () => {
-    const gsid = uuidv4();
-    socket.join(gsid);
+  socket.on('create_room', (payload: IWebRTCPayload) => {
+    const room = getRoom(io, payload.gsid);
+    validateRoomCreate(socket, room);
+    socket.join(payload.gsid);
+    logRoomStatus({
+      roomList: getRoomList(io),
+      room: getRoom(io, payload.gsid),
+      type: LogType.create,
+      gsid: payload.gsid,
+    });
   });
 
   socket.on('join_room', (payload: IWebRTCPayload) => {
-    const room = io.sockets.adapter.rooms.get(payload.gsid);
+    const room = getRoom(io, payload.gsid);
     validateRoomJoin(socket, room);
     socket.join(payload.gsid);
+    logRoomStatus({
+      roomList: getRoomList(io),
+      room: getRoom(io, payload.gsid),
+      type: LogType.join,
+      gsid: payload.gsid,
+    });
   });
 };
 
@@ -23,6 +37,13 @@ const validateRoomJoin = (socket: Socket, room: Set<string> | undefined) => {
   if (room && room.size >= ROOM_CONSTANTS.maxParticipants) errorMessage = ROOM_ERROR_MESSAGES.full;
   socket.emit('error', {
     errorMessage,
+  });
+};
+
+const validateRoomCreate = (socket: Socket, room: Set<string> | undefined) => {
+  if (!room) return;
+  socket.emit('error', {
+    errorMessage: ROOM_ERROR_MESSAGES.alreadyExists,
   });
 };
 
