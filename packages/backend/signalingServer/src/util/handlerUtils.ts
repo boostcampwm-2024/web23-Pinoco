dbda90152ae5a4a46de1de6d7d176adb5d518aab
today-is-first <diligent0644@gmail.com>
2024-11-26 12:39:43 +0900

    feat: io.sockets에서 원하는 소켓 가져오는 기능 구현

--- packages/backend/signalingServer/src/util/handlerUtils.ts
+++ packages/backend/signalingServer/src/util/handlerUtils.ts
@@ -7,3 +7,8 @@ export const getRoom = (io: Server, gsid: string) => {
 export const getRoomList = (io: Server) => {
   return io.sockets.adapter.rooms;
 };
+
+export const getTargetSocket = async (io: Server, userId: string) => {
+  const sockets = await io.fetchSockets();
+  return sockets.find((socket) => socket.data.userId === userId);
+};
