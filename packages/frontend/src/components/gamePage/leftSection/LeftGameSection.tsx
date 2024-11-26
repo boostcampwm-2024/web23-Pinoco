d00b4e15c83c621dfaa2d2ec35df452c99b8ae38
today-is-first <diligent0644@gmail.com>
2024-11-26 12:45:56 +0900

    feat: 스트림 없는 사용자는 빈 슬롯으로 보이게 수정

--- packages/frontend/src/components/gamePage/leftSection/LeftGameSection.tsx
+++ packages/frontend/src/components/gamePage/leftSection/LeftGameSection.tsx
@@ -1,12 +1,25 @@
 import VideoFeed from './VideoFeed';
 import MainDisplay from './MainDisplay';
+import { useLocalStreamStore } from '@/states/store/localStreamStore';
+import { useAuthStore } from '@/states/store/authStore';
+import { usePeerConnectionStore } from '@/states/store/peerConnectionStore';
 
 export default function LeftGameSection() {
+  const { localStream } = useLocalStreamStore();
+  const remoteStreams = usePeerConnectionStore((state) => state.remoteStreams);
+  const { userId } = useAuthStore();
+
   return (
     <div className="flex flex-col w-2/3 p-4 space-y-4 bg-transparent">
       <div className="grid grid-cols-3 gap-4">
-        {[...Array(6)].map((_, idx) => (
-          <VideoFeed key={idx} userName={`참가자 ${idx + 1}`} />
+        <VideoFeed stream={localStream} userName={userId} isLocal={true} />
+
+        {Array.from(remoteStreams).map(([userId, stream]) => (
+          <VideoFeed key={userId} stream={stream} userName={userId} />
+        ))}
+
+        {[...Array(Math.max(0, 5 - remoteStreams.size))].map((_, idx) => (
+          <VideoFeed key={`empty-${idx}`} stream={null} userName={`빈 슬롯 ${idx + 1}`} />
         ))}
       </div>
       <MainDisplay />
