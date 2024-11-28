e55f0eb161eadf5e5f61c45fcd6cd301be748778
today-is-first <diligent0644@gmail.com>
2024-11-28 14:55:23 +0900

    chore: 경로 변경

--- packages/frontend/src/components/gamePage/rightSection/SettingSection.tsx
+++ packages/frontend/src/components/gamePage/rightSection/SettingSection.tsx
@@ -1,5 +1,5 @@
-import CameraSettingButton from '@/components/gamePage/rightSection/CameraSettingButton';
-import MikeSettingButton from '@/components/gamePage/rightSection/MikeSettingButton';
+import CameraSettingButton from '@/components/gamePage/stream/CameraSettingButton';
+import MikeSettingButton from '@/components/gamePage/stream/MikeSettingButton';
 import LeaveButton from '@/components/gamePage/rightSection/LeaveButton';
 
 export default function SettingSection() {
