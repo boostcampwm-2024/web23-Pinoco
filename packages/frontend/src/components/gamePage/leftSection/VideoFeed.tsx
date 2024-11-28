0c4925d7e52247e944c5c80b533f153f0f061378
today-is-first <diligent0644@gmail.com>
2024-11-28 11:09:10 +0900

    refactor: 사용자 비디오 비율 개선

--- packages/frontend/src/components/gamePage/leftSection/VideoFeed.tsx
+++ packages/frontend/src/components/gamePage/leftSection/VideoFeed.tsx
@@ -8,15 +8,27 @@ export default function VideoFeed() {
   const localStream = useLocalStreamStore((state) => state.localStream);
   const remoteStreams = usePeerConnectionStore((state) => state.remoteStreams);
   const { userId } = useAuthStore();
+  const feedHeight = 'h-[180px]';
 
   return (
     <>
-      <VideoStream stream={localStream} userName={userId} isLocal={true} />
+      <VideoStream stream={localStream} userName={userId} isLocal={true} height={feedHeight} />
       {Array.from(remoteStreams).map(([userId, stream]) => (
-        <VideoStream key={userId} stream={stream} userName={userId} isLocal={false} />
+        <VideoStream
+          key={userId}
+          stream={stream}
+          userName={userId}
+          isLocal={false}
+          height={feedHeight}
+        />
       ))}
       {[...Array(Math.max(0, 5 - remoteStreams.size))].map((_, idx) => (
-        <VideoStream key={`empty-${idx}`} stream={null} userName={`빈 슬롯 ${idx + 1}`} />
+        <VideoStream
+          key={`empty-${idx}`}
+          stream={null}
+          userName={`빈 슬롯 ${idx + 1}`}
+          height={feedHeight}
+        />
       ))}
     </>
   );
