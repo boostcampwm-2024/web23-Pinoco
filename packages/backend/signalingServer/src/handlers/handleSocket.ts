30a53e5c3216a6750ad9f89524cf9e245a6bcd1e
today-is-first <diligent0644@gmail.com>
2024-12-02 14:00:48 +0900

    feat: Socket.io disconnect 이벤트 발생 시 user_left 구현

--- packages/backend/signalingServer/src/handlers/handleSocket.ts
+++ packages/backend/signalingServer/src/handlers/handleSocket.ts
@@ -3,6 +3,9 @@ import { SOCKET_MESSAGES } from '@/constants/socketConstants';
 
 const handleSocket = (socket: Socket) => {
   socket.on('disconnect', () => {
+    socket
+      .to(socket.data.gsid)
+      .emit('user_left', { fromUserId: socket.id, gsid: socket.data.gsid });
     console.log(SOCKET_MESSAGES.disconnect);
   });
 };
