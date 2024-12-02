052633d1f33c292fe794cb45170d3d7dd77c50f7
today-is-first <diligent0644@gmail.com>
2024-12-02 18:52:58 +0900

    feat: LocalStream에 CurrentMedia / Device 목록 추가

--- packages/frontend/src/store/localStreamStore.ts
+++ packages/frontend/src/store/localStreamStore.ts
@@ -2,10 +2,26 @@ import { create } from 'zustand';
 
 interface ILocalStreamState {
   localStream: MediaStream | null;
+  videoDevices: MediaDeviceInfo[];
+  audioInputDevices: MediaDeviceInfo[];
+  currentVideoDevice: MediaDeviceInfo | null;
+  currentAudioDevice: MediaDeviceInfo | null;
   setLocalStream: (stream: MediaStream | null) => void;
+  setVideoDevices: (devices: MediaDeviceInfo[]) => void;
+  setAudioInputDevices: (devices: MediaDeviceInfo[]) => void;
+  setCurrentVideoDevice: (device: MediaDeviceInfo | null) => void;
+  setCurrentAudioDevice: (device: MediaDeviceInfo | null) => void;
 }
 
 export const useLocalStreamStore = create<ILocalStreamState>((set) => ({
   localStream: null,
+  videoDevices: [],
+  audioInputDevices: [],
+  currentVideoDevice: null,
+  currentAudioDevice: null,
   setLocalStream: (stream) => set({ localStream: stream }),
+  setVideoDevices: (devices) => set({ videoDevices: devices }),
+  setAudioInputDevices: (devices) => set({ audioInputDevices: devices }),
+  setCurrentVideoDevice: (device) => set({ currentVideoDevice: device }),
+  setCurrentAudioDevice: (device) => set({ currentAudioDevice: device }),
 }));
