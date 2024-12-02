6643d118748898b6b45a263ef566e53b3eeb45e9
zizonyoungjun <naya700@naver.com>
2024-12-03 00:55:59 +0900

    fix: 게임 엔딩 로직 수정

--- packages/frontend/src/components/gamePage/leftSection/GamePhases/EndingResult.tsx
+++ packages/frontend/src/components/gamePage/leftSection/GamePhases/EndingResult.tsx
@@ -15,38 +15,44 @@ export default function EndingResult({ endingResult }: IEndingPhaseProps) {
   const resultImage = isPinocoWin
     ? '/images/ending/pinocoWin.png'
     : '/images/ending/geppettoWin.png';
+
   const getEndingMessage = () => {
-    if (guessingWord && isGuessed) {
-      return (
-        <>
-          피노코가 제시어를 맞추며 위기에서 벗어났습니다! 제출된 제시어:{' '}
-          <span className="font-extrabold">{guessingWord}</span>
-        </>
-      );
+    if (isGuessed) {
+      if (isPinocoWin) {
+        return (
+          <>
+            피노코가 제시어를 맞추며 위기에서 벗어났습니다! 제출된 제시어 :{' '}
+            <span className="font-extrabold">{guessingWord}</span>
+          </>
+        );
+      } else {
+        return (
+          <>
+            투표로 지목된 피노코가 제시어를 맞추지 못했습니다! 제출된 제시어 :{' '}
+            <span className="font-extrabold">{guessingWord}</span>
+          </>
+        );
+      }
     }
-
-    if (isPinocoWin) {
-      return <>피노코가 자신의 정체를 숨기며 제페토를 속였습니다 😉</>;
-    }
-
-    return <>피노코가 제시어를 맞추지 못했습니다 🔨</>;
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-full space-y-8">
-      <img
-        src={resultImage}
-        alt={isPinocoWin ? '피노코 승리' : '제페토 승리'}
-        className="w-[60%] max-h-[400px] object-contain"
-      />
+    <div className="flex flex-col items-center justify-center h-full space-y-6 overflow-hidden">
+      <div className="w-full max-w-sm">
+        <img
+          src={resultImage}
+          alt={isPinocoWin ? '피노코 승리' : '제페토 승리'}
+          className="w-full h-auto object-contain"
+        />
+      </div>
 
-      <h2 className="text-3xl font-extrabold text-white-default">
+      <h2 className="text-2xl font-extrabold text-white-default text-center">
         {isPinocoWin ? '피노코가 승리했습니다 🤥' : '제페토가 승리했습니다 🧓🏻'}
       </h2>
 
-      <div className="text-2xl text-center text-white-default leading-relaxed">
+      <div className="text-lg text-center text-white-default leading-relaxed px-4">
         <p>
-          피노코는 <span className="font-extrabold">{pinoco}</span> 였습니다!
+          피노코는 <span className="font-extrabold">{pinoco}</span>였습니다!
         </p>
         <p>{getEndingMessage()}</p>
       </div>
