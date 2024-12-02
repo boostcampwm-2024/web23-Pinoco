4b792f0c5ff70c0e26889ca0e364febb3cb2dddd
zizonyoungjun <naya700@naver.com>
2024-12-02 23:48:47 +0900

    feat: roomStore에서 readyUser를 전역 관리하는 코드 추가

--- packages/frontend/src/store/roomStore.ts
+++ packages/frontend/src/store/roomStore.ts
@@ -1,16 +1,21 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+
 interface IRoomState {
   isHost: boolean;
   gsid: string | null;
   isPinoco: boolean;
   allUsers: Set<string>;
+  readyUsers: string[];
   setRoomData: (gsid: string | null, isHost: boolean, isPinoco: boolean) => void;
   setIsPinoco: (isPinoco: boolean) => void;
   setAllUsers: (allUsers: string[]) => void;
   addUser: (userId: string) => void;
   removeUser: (userId: string) => void;
   setIsHost: (isHost: boolean) => void;
+  setReadyUsers: (readyUsers: string[]) => void;
+  addReadyUser: (userId: string) => void;
+  removeReadyUser: (userId: string) => void;
 }
 export const useRoomStore = create<IRoomState>()(
   persist(
@@ -19,6 +24,8 @@ export const useRoomStore = create<IRoomState>()(
       gsid: null,
       isPinoco: false,
       allUsers: new Set(),
+      readyUsers: [],
+
       setRoomData: (gsid, isHost, isPinoco) => set({ gsid, isHost, isPinoco }),
       setIsPinoco: (isPinoco) => set({ isPinoco }),
       setAllUsers: (allUsers) => set({ allUsers: new Set(allUsers) }),
@@ -31,6 +38,16 @@ export const useRoomStore = create<IRoomState>()(
           allUsers: new Set([...state.allUsers].filter((id) => id !== userId)),
         })),
       setIsHost: (isHost) => set({ isHost }),
+
+      setReadyUsers: (readyUsers) => set({ readyUsers }),
+      addReadyUser: (userId) =>
+        set((state) => ({
+          readyUsers: [...state.readyUsers, userId],
+        })),
+      removeReadyUser: (userId) =>
+        set((state) => ({
+          readyUsers: state.readyUsers.filter((id) => id !== userId),
+        })),
     }),
     { name: 'room-storage' },
   ),
