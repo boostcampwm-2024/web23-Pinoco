bba6c4ab73eccc49e6231591958abbd8f8be9a9b
Cllaude99 <xodbsrla1025@naver.com>
2024-12-03 19:36:20 +0900

    refactor: code spliting을 통한 FCP 개선

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,46 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
-import { useEffect } from 'react';
+import { Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense, useEffect } from 'react';
+import Layout from '@/components/layout/Layout';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+import PublicRoute from '@/components/PublicRoute';
+import PrivateRoute from '@/components/PrivateRoute';
+
+const LandingPage = lazy(() => import('@/pages/landingPage/index'));
+const LobbyPage = lazy(() => import('@/pages/lobbyPage/index'));
+const GamePage = lazy(() => import('@/pages/gamePage/index'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <PublicRoute>
+            <LandingPage />
+          </PublicRoute>
+        ),
+      },
+      {
+        path: 'lobby',
+        element: (
+          <PrivateRoute>
+            <LobbyPage />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'game/:gsid',
+        element: (
+          <PrivateRoute>
+            <GamePage />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
 
 function App() {
   useEffect(() => {
@@ -10,16 +50,15 @@ function App() {
     };
 
     window.addEventListener('beforeunload', handleUnload);
-
     return () => {
       window.removeEventListener('beforeunload', handleUnload);
     };
   }, []);
 
   return (
-    <>
+    <Suspense fallback={<LoadingSpinner />}>
       <RouterProvider router={router} />
-    </>
+    </Suspense>
   );
 }
 
