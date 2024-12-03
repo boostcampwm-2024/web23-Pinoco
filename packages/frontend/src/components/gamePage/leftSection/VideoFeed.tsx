b5826d8e840af2e68e7922b89d2df27d3b58ab19
zizonyoungjun <naya700@naver.com>
2024-12-03 18:11:27 +0900

    feat: 비디오피드에 방장 표시 추가

--- packages/frontend/src/components/gamePage/leftSection/VideoFeed.tsx
+++ packages/frontend/src/components/gamePage/leftSection/VideoFeed.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import VideoStream from '@/components/gamePage/stream/VideoStream';
 import { useAuthStore } from '@/store/authStore';
 import { useLocalStreamStore } from '@/store/localStreamStore';
@@ -11,15 +10,20 @@ export default function VideoFeed() {
   const { userId } = useAuthStore();
   const feedHeight = 'h-[180px]';
   const readyUsers = useRoomStore((state) => state.readyUsers);
+  const hostUserId = useRoomStore((state) => state.hostUserId);
   const isUserReady = (userId: string) => readyUsers.includes(userId);
+  const isHost = (userId: string) => userId === hostUserId;
 
   return (
     <>
       <div
-        className={`relative rounded-lg`}
-        style={{
-          boxShadow: isUserReady(userId || '') ? '0 0 0 4px white' : '',
-        }}
+        className={`relative rounded-lg ${
+          isHost(userId || '')
+            ? 'border-4 border-blue-500'
+            : isUserReady(userId || '')
+              ? 'border-4 border-white'
+              : ''
+        }`}
       >
         <VideoStream stream={localStream} userName={userId} isLocal={true} height={feedHeight} />
         {isUserReady(userId || '') && (
@@ -27,13 +31,23 @@ export default function VideoFeed() {
             <span className="px-2 py-1 text-orange-500 font-bold bg-white rounded-full">READY</span>
           </div>
         )}
+        {isHost(userId || '') && (
+          <div className="absolute top-2 right-2">
+            <span className="px-2 py-1 text-blue-500 font-bold bg-white rounded-full">HOST</span>
+          </div>
+        )}
       </div>
+
       {Array.from(remoteStreams).map(([remoteUserId, stream]) => (
         <div
           key={remoteUserId}
-          className={`relative ${
-            isUserReady(remoteUserId) ? 'border-4 border-white' : ''
-          } rounded-lg`}
+          className={`relative rounded-lg ${
+            isHost(remoteUserId)
+              ? 'border-4 border-blue-500'
+              : isUserReady(remoteUserId)
+                ? 'border-4 border-white'
+                : ''
+          }`}
         >
           <VideoStream
             stream={stream}
@@ -48,8 +62,14 @@ export default function VideoFeed() {
               </span>
             </div>
           )}
+          {isHost(remoteUserId) && (
+            <div className="absolute top-2 right-2">
+              <span className="px-2 py-1 text-blue-500 font-bold bg-white rounded-full">HOST</span>
+            </div>
+          )}
         </div>
       ))}
+
       {[...Array(Math.max(0, 5 - remoteStreams.size))].map((_, idx) => (
         <VideoStream
           key={`empty-${idx}`}
